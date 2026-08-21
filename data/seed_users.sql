bfa1b761bf6826badd04cae09a7a1ebaede2cfa4
Ubuntu <ubuntu@ip-172-31-31-95.ec2.internal>
2026-08-21 20:13:58 +0000

    Add new users

--- data/seed_users.sql
+++ data/seed_users.sql
@@ -1,14 +1,23 @@
--- Usuarios de prueba generados automáticamente (prefijos: usuario, usa).
+-- Usuarios de prueba generados automáticamente (prefijos: investigador, semillero, colaborador, usa).
 -- Password en texto plano NO se guarda aquí; ver el archivo de credenciales.
 INSERT INTO users (id, email, password_hash, display_name, role, created_at) VALUES
-    ('dedfb3ec-7681-47f9-81dd-a6de9e9b2aaa', 'usuario1@atlas.local', '$2b$12$1OxMdKcp39.2JC9ehb/CxO4grvrlCh9ZYkuMEsrf0Xmpqwbsyp4fS', 'Usuario 1', 'user', '2026-07-03T20:20:34.566362+00:00'),
-    ('2d2b624c-c881-4626-a4c2-54b5781932d3', 'usuario2@atlas.local', '$2b$12$xBKVjI1GD8BGVoqatUBXnONi4kFThsLEDdLPJU78gWgwOnvb7IuOy', 'Usuario 2', 'user', '2026-07-03T20:20:34.566362+00:00'),
-    ('0f1629ca-9d6e-47bd-8c67-3e37fbc92cff', 'usuario3@atlas.local', '$2b$12$yvIK8AjqNK4WOC7lF2pc9OlITEPkk58Eul2knF8fkcW58zA9dgIAu', 'Usuario 3', 'user', '2026-07-03T20:20:34.566362+00:00'),
-    ('11689575-d340-4a03-8ec2-c7753bab09bd', 'usuario4@atlas.local', '$2b$12$p.fcsFKr4BA3PCCclzWHYuYWbxeL8a9wyEDtZ41tqk9IQicargE7W', 'Usuario 4', 'user', '2026-07-03T20:20:34.566362+00:00'),
-    ('fe56ab4a-fd11-4564-9110-d15d3d403555', 'usuario5@atlas.local', '$2b$12$H393AweOkIvaStvIC21OWucD0pBbHkA5paHhUICfWnKbZibIwIaJy', 'Usuario 5', 'user', '2026-07-03T20:20:34.566362+00:00'),
-    ('797e62a9-0872-4fa5-ad4e-f4b0fabf7ca2', 'usa1@atlas.local', '$2b$12$B/jwSN1LsSlQto.lGMFGcuHeFnc4t99LfzJYcFj2trQOxJkwsgHPu', 'Usa 1', 'user', '2026-07-03T20:20:34.566362+00:00'),
-    ('4e2999c3-637e-4c36-8a38-085521560f13', 'usa2@atlas.local', '$2b$12$yv2DMYmSI85clqMhDR8ODenRBrkAjpg/InIby9ZzAfiqQx7uGg1Ty', 'Usa 2', 'user', '2026-07-03T20:20:34.566362+00:00'),
-    ('d09ab57d-9909-4522-95d4-fc6838b84478', 'usa3@atlas.local', '$2b$12$6w5GVbTnC5mxY7055lDEduIPTsJAyaet31H./AqsQ7prXpyTq/tXi', 'Usa 3', 'user', '2026-07-03T20:20:34.566362+00:00'),
-    ('cfea398e-6011-4fe0-8708-a5f3818f9571', 'usa4@atlas.local', '$2b$12$rQZ8NX2IvzslzF/lL477OuWFkTowEsp7jIIl8vJhuGYLrGoSVwPiW', 'Usa 4', 'user', '2026-07-03T20:20:34.566362+00:00'),
-    ('964b7358-1eb7-465f-9d28-e16419708dbc', 'usa5@atlas.local', '$2b$12$swl0NPovYcvknn/jc0OGAej3wjCV0tlWaxIqxN0G9n8LZcQBjrEOu', 'Usa 5', 'user', '2026-07-03T20:20:34.566362+00:00')
+    ('a9b74040-cf2e-4018-9d2d-c9d6cd1d1c7d', 'investigador1@atlas.local', '$2b$12$oJA2//vbWTKdGBu34xL8keCXi3QKDJ/z0NWdRhnz3Q9XHB9zo2NhK', 'Investigador 1', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('0792dc4a-2282-4397-8e9b-636c91fe2264', 'investigador2@atlas.local', '$2b$12$1REBR0Zev41BPE0WP815wuy1p809VQ7aWcuff/9PqULIQg4LjSmZW', 'Investigador 2', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('1a2f8f9c-4de3-4c60-93ac-14cb4263af09', 'investigador3@atlas.local', '$2b$12$8LrDbr31tYWwiFQKc52oGuheWOZydvQ5fBImqsMD.G515laTxe81O', 'Investigador 3', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('659c5625-ae91-4624-89d2-317123c65cac', 'investigador4@atlas.local', '$2b$12$IbllwJjzrC6CnHo9rBgbwuALaur69bKuMEn6cWKffAjIlVuLHR9.i', 'Investigador 4', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('5f2a968d-3f99-425e-8537-cfc4d1ea0141', 'semillero1@atlas.local', '$2b$12$TvWDThqX8SUQ4kRXkcRQmuzYPwYMoOYWYznVqdUbxhjewEYVfKSxa', 'Semillero 1', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('4d4881a1-2a9d-49df-958d-1f686fbd66c8', 'semillero2@atlas.local', '$2b$12$rXZaLsVD/oSSeljCprRuRO8ig92gCZsIJIUTC9DWc6shUKZiFHQ5.', 'Semillero 2', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('78048ef1-337c-4e31-a6e9-e6a71b39287c', 'semillero3@atlas.local', '$2b$12$2bb6EjPi2kNp.OR/q8g8kOYX.BfjT.F4yq7Ry5eS3fmgyIH2aNwai', 'Semillero 3', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('f50e1361-5361-4118-a3a0-e2afae33f936', 'semillero4@atlas.local', '$2b$12$gMVuae4bc26ZUuHLGU8KduV4AbUHpRNNfadND8fd6aCuAgWEg/tGS', 'Semillero 4', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('6e8b76d2-1839-426e-a709-f08d46a41ef5', 'semillero5@atlas.local', '$2b$12$cqtBtm6wnVfd0YpT7FtaV.7wej2nP6snUcYcmgH9k1aoCHkzGHkpy', 'Semillero 5', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('21b64622-cbd0-4e65-9dd3-8b7ceeb00b03', 'semillero6@atlas.local', '$2b$12$Jeaay.QqxnM4mGh5xxDMqOYj7ES7TZBp5wlZ7WBoBVykNLDTVbBIC', 'Semillero 6', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('7216b7e1-728e-41a9-b265-26452b524e8b', 'colaborador1@atlas.local', '$2b$12$dS/0ejNT9zl3gpSUH.DYQ.DEZv6hw92yR3sPvb7dmM8.2uoEtWZe6', 'Colaborador 1', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('6f9d7663-81b7-4d93-a2ee-f7a4cff7f5fb', 'colaborador2@atlas.local', '$2b$12$npj2SzIuwT7TY0pcLuI3neqlzGORBEsz2Y6C1MphpHuSZlARzEKFC', 'Colaborador 2', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('ff14002c-03dc-42d1-a3f4-8dfd4c1d5a4d', 'colaborador3@atlas.local', '$2b$12$jLuHLgKxaDF4mD9FMr2mQeCfJTjaGqDtYcCLRM1hMwNcMtB0qVxmW', 'Colaborador 3', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('450beeb2-39ff-4148-bc43-95b1c596ba90', 'colaborador4@atlas.local', '$2b$12$b9ug4jey5cAlhUnLOPXhuOs3fN1e/xFOBE5T0YNB5cNtGLPtWGUCS', 'Colaborador 4', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('8411057a-faae-4d3a-93d1-ebf538385621', 'usa1@atlas.local', '$2b$12$T4ZGrj6dX9bjLbuUomZb7uTyqyaHu/84GsLDa3/kbdoNp4tGGcm7a', 'Usa 1', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('3d10b0f1-c8bf-471d-97d8-d3146fa743cf', 'usa2@atlas.local', '$2b$12$WSaJanMVr2uin9PBiJ7oy.5XMR/ME9HF723.U48mbOu5HzkV1d0Jq', 'Usa 2', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('4ae4366b-860e-4487-ac3f-7141787c5167', 'usa3@atlas.local', '$2b$12$OplmlWCkycf74bLH1ha01ObVmi6ZOlPvztkDG0QQ6Yf4gyZsKvTxm', 'Usa 3', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('88d2b8c3-0578-4579-9ea5-ca333ccc68f9', 'usa4@atlas.local', '$2b$12$TQLc27pQEumeTa9lXTrnpOGQbUyfjmN6V1Atou1W3CGUk0ObmajYK', 'Usa 4', 'user', '2026-08-21T15:31:07.228109+00:00'),
+    ('72a24777-8d74-47b5-8dc1-9268256e53fd', 'usa5@atlas.local', '$2b$12$581Zhwq6s4J26kn3DNrkieMVoJI4gVBk5.JAfs51tpHgmdyjJ4OjK', 'Usa 5', 'user', '2026-08-21T15:31:07.228109+00:00')
 ON CONFLICT DO NOTHING;
